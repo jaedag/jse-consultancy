100bf3a7de5539ac9577e25bed305d5f6890201a
ohenekwabena <adjeiasarejesse@gmail.com>
2024-11-12 21:20:49 +0000

    Fix nav modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,30 +5,30 @@ import { ModalTrigger, ModalBody, ModalContent, ModalProvider } from "./modal";
 
 export default function Header() {
     return (
-        <header className="w-full flex justify-between p-4 items-center bg-blue-400 fixed top-0 z-10">
+        <header className="w-full flex justify-between p-4 items-center bg-blue-400 fixed top-0 z-10 text-white">
             <h1>Logo</h1>
             <Spacer />
-            <nav className="hidden md:flex gap-4">
-                    <Link href={"/"}>Home</Link>
-                    <Link href={"/"}>Our Services</Link>
-                    <Link href={"/"}>About Us</Link>
-                    <Link href={"/"}>Publications</Link>
+            <nav className="hidden md:flex gap-6">
+                <Link href={"/"}>Home</Link>
+                <Link href={"/"}>Our Services</Link>
+                <Link href={"/"}>About Us</Link>
+                <Link href={"/"}>Publications</Link>
             </nav>
             <Spacer />
             <ModalProvider>
-            <ModalTrigger className="p-4 rounded-full bg-gray-200 md:hidden">
-            <GiHamburgerMenu />
-            </ModalTrigger>
-            <ModalBody className=" rounded-2xl mx-4">
-                <ModalContent className = " sm:max-w-[40%] w-5">
-                <nav className="flex gap-6 flex-col justify-center grow w-fit">
-                    <Link href={"/"}>Home</Link>
-                    <Link href={"/"}>Our Services</Link>
-                    <Link href={"/"}>About Us</Link>
-                    <Link href={"/"}>Publications</Link>
-            </nav>
-                </ModalContent>
-            </ModalBody>
+                <ModalTrigger className="p-4 rounded-full bg-gray-200 md:hidden">
+                    <GiHamburgerMenu />
+                </ModalTrigger>
+                <ModalBody className=" rounded-2xl mx-4 text-black w-[60%] items-center">
+                    <ModalContent className="justify-center">
+                        <nav className="flex gap-6 flex-col justify-center grow w-fit align-center">
+                            <Link href={"/"}>Home</Link>
+                            <Link href={"/"}>Our Services</Link>
+                            <Link href={"/"}>About Us</Link>
+                            <Link href={"/"}>Publications</Link>
+                        </nav>
+                    </ModalContent>
+                </ModalBody>
             </ModalProvider>
         </header>
     )
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -104,7 +104,7 @@ export const ModalBody = ({
           <motion.div
             ref={modalRef}
             className={cn(
-              "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
+              "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col md:flex-1 overflow-hidden",
               className
             )}
             initial={{
@@ -147,7 +147,7 @@ export const ModalContent = ({
   className?: string;
 }) => {
   return (
-    <div className={cn("flex flex-col flex-1 p-8 md:p-10", className)}>
+    <div className={cn("flex flex-col flex-1 p-8 md:p-10 w-fit", className)}>
       {children}
     </div>
   );
